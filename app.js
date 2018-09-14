71fcf4b2f03977fd855def8e91b35f156b5d4c92
root <root@vps567361.ovh.net>
2018-09-14 02:55:20 +0200

    ajout des formulaires et de l'authentification

--- app.js
+++ app.js
@@ -5,19 +5,15 @@ const helmet = require('helmet')
 const mysql = require('mysql')
 const superagent = require('superagent')
 const sha=require('sha1')
-const multer = require('multer')
-var storage  = multer.diskStorage({
-	destination: function (req, file, callback) {
-	  callback(null, '/asset/docs');
-	},
-	filename: function (req, file, callback) {
-	  callback(null, file.fieldname);
-	}
-  });
-var upload = multer({ storage : storage}).single('userPhoto');
+const session = require('express-session')
+var sess
+app.use(session({secret:'TokenAleaToire',connected:false}))
+var user=process.env.SMS_USER;
+var pass=process.env.SMS_PASS;
+
 function sendsms(content){
 	superagent.post('https://smsapi.free-mobile.fr/sendmsg')
-		.send({user:process.env.SMS_USER,pass:process.env.SMS_PASS,msg:content})
+		.send({user:user,pass:pass,msg:content})
 		.end(function(err, res) {
 		console.log('Message envoyé le '+ new Date()+" evt = "+content )
 	});
@@ -29,7 +25,7 @@ function getMysqlConnection(){
 		password:process.env.DB_USER_PWD,
 		database:process.env.DB_NAME})
 }
-app.use(helmet())
+
 app.use(express.static(__dirname))
 
 app.use(bodyParser.urlencoded({
@@ -45,59 +41,73 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Credentials', true)
     next()
 })
+app.use(helmet())
+
 
 app.get('/login', (req, res) => {
-	sendsms('login url hit')
-    res.sendFile(__dirname+ '/views/login.html')
+	sendsms("Login form reached")
+	let ip = req.connection.remoteAddress.split(':')[3]
+	let token=sha(ip)
+	res.render(__dirname+ '/views/login.pug',{token:token})
 })
-app.post('/formulaire', (req, res) => {
-	sendsms('formulaire url hit')
-	// let ip=req.connection.remoteAddress.split(':')[3]
-	// let token = sha(ip)
-	// connection=getMysqlConnection()
-	// connection.connect()
-    // let sqlQuery = 'select id, libelle,nature,chemin,lienhttp, DATE_FORMAT(datereation, "%d/%m/%Y")datecreation,miniature,UPPER(categorie) as categ from document where true'
-    // connection.query(sqlQuery,function(err,result){
-	// 			if(err) 
-	// 				throw err
-	// 			else
-	// 				res.render(__dirname+ '/views/formulaire.pug',
-	// 						{
-	// 						token:token,
-	// 						list:result,
-	// 						widthValue:'10%',
-	// 						titre:'Alter-Egaux : Formulaire'
-	// 						}
-	// 					)
-	// 				})
-	// connection.end()
-	res.render(__dirname+ '/views/formulaire.pug',
-							{
-							token:'token',
-							list:[],
-							widthValue:'10%',
-							titre:'Alter-Egaux : Formulaire'
-							}
-						)
+app.get('/partners', (req, res) => {
+	//sendsms('partners url hit')
+    res.render(__dirname+ '/views/partners.pug',
+				{
+				paddingvalue:"body {padding-top:5rem;}}",
+				widthValue:'10%',
+				titre:'Alter-Egaux : Partenaires'
+				}
+			)
 })
+// app.post('/adddocodd', (req, res) => {
+// 	sendsms('formulaire url hit')
+// 	// let ip=req.connection.remoteAddress.split(':')[3]
+// 	// let token = sha(ip)
+// 	// connection=getMysqlConnection()
+// 	// connection.connect()
+//     // let sqlQuery = 'select id, libelle,nature,chemin,lienhttp, DATE_FORMAT(datereation, "%d/%m/%Y")datecreation,miniature,UPPER(categorie) as categ from document where true'
+//     // connection.query(sqlQuery,function(err,result){
+// 	// 			if(err) 
+// 	// 				throw err
+// 	// 			else
+// 	// 				res.render(__dirname+ '/views/formulaire.pug',
+// 	// 						{
+// 	// 						token:token,
+// 	// 						list:result,
+// 	// 						widthValue:'10%',
+// 	// 						titre:'Alter-Egaux : Formulaire'
+// 	// 						}
+// 	// 					)
+// 	// 				})
+// 	// connection.end()
+// 	res.render(__dirname+ '/views/adddocodd.pug',
+// 							{
+// 							token:'token',
+// 							list:[],
+// 							widthValue:'10%',
+// 							titre:'Alter-Egaux : Mise à jour odd'
+// 							}
+// 						)
+// })
 app.get('/list/:categoryId', (req, res) => {
 	sendsms('list document'+ req.params.categoryId +'  url hit')
 	if(req.params.categoryId){
 		connection=getMysqlConnection()
 		connection.connect()
-                let param=req.params.categoryId
-                let sqlQuery = 'select id, libelle,nature,chemin,lienhttp, DATE_FORMAT(datereation, "%d/%m/%Y")datecreation  from document where categorie=? and true'
-                connection.query(sqlQuery,[param],function(err,result){
-                        if(err) 
-				throw err
-			else
-				res.render(__dirname+ '/views/list.pug',
-	                	{
-        	        	list:result,
-                		titre:'Alter-Egaux : Liste '+param,
-	                	lien:param,
-        	        	widthValue:'10%'})
-               	})
+		let param=req.params.categoryId
+		let sqlQuery = 'select id, libelle,nature,chemin,lienhttp, DATE_FORMAT(datereation, "%d/%m/%Y")datecreation  from document where categorie=? and true'
+		connection.query(sqlQuery,[param],function(err,result){
+		if(err) 
+			throw err
+		else
+			res.render(__dirname+ '/views/list.pug',
+					{
+					list:result,
+					titre:'Alter-Egaux : Liste '+param,
+					lien:param,
+					widthValue:'10%'})
+			})
 		connection.end()
         }
 })
@@ -142,40 +152,208 @@ app.get('/histoire',(req,res)=>{
 })
 app.get('/outils',(req,res)=>{
 	sendsms('outils url hit')
-	res.render(__dirname+ '/views/outils.pug',
+	connection=getMysqlConnection()
+	connection.connect()
+	let sqlQuery = 'select libelle,description,lien,apercu,DATE_FORMAT(dateajout, "%d/%m/%Y")dateajout from outil where true'
+	connection.query(sqlQuery,function(err,result){
+		if(err) 
+			throw err
+		else
+			res.render(__dirname+ '/views/outils.pug',
 			{
+				list:result,
 				widthValue:'10%',
 				paddingvalue:"body {padding-top:5rem;}",
 				titre:'Alter-Egaux : Outils de communication'
 			}
+		)
+	})
+	
+})
+app.get('/',(req,res)=>{
+	res.render(__dirname+ '/index.pug',
+			{
+				widthValue:'20%',
+				paddingvalue:"body {padding-top:5rem;}",
+				titre:'Alter-Egaux : Accueil'
+			}
 		);
 })
-
-app.post('/savenewdoc',(req,res)=>{
-	sendsms('save new doc url hit')
-	upload(req,res,function(err) {
-        if(err) {
-            return res.end("Error uploading file.");
-        }
-        res.end("File is uploaded");
-    });
-	//console.log(req.socket.address())
-	// let ip=req.connection.remoteAddress.split(':')[3]
-	// let token=sha(ip)
-	console.log(req.files)
-	console.log(req.body)
-	res.send("ok")
-		/*let param=req.params.categoryId
-		let sqlQuery = "select * from document where categorie=? and true" 
-
-		connection.query(sqlQuery,[param],function(err,result){
-			if(err) throw err;
-			console.log("result : ",result)
-		})*/
+app.get('/add/:element',(req,res)=>{//root les url pour les formulaires
+	sess = req.session
+	if(sess.connected){
+		if(req.params.element){
+			let element=req.params.element // tool ou event
+			let ip = req.connection.remoteAddress.split(':')[3]
+			let token=sha(ip)
+			if (element=='tool') {
+				connection=getMysqlConnection()
+				connection.connect()
+				let sqlQuery = 'select libelle,description,lien,apercu,DATE_FORMAT(dateajout, "%d/%m/%Y")dateajout from outil where true'
+				connection.query(sqlQuery,function(err,result){
+					if(err) 
+						throw err
+					else
+						res.render(__dirname+ '/views/addtool.pug',
+							{
+								list:result,
+								token:token,
+								widthValue:'10%',
+								paddingvalue:"body {padding-top:5rem;}",
+								titre:'Alter-Egaux : Mise à jour outils'
+							})
+				})
+			}
+			else
+			if (element=='event') {
+				connection=getMysqlConnection()
+				connection.connect()
+				let param=req.params.categoryId
+				let sqlQuery = 'select cheminminiature, cheminaffiche, libelle, lien  from evenement where true'
+				connection.query(sqlQuery,[param],function(err,result){
+					if(err) 
+						throw err
+					else
+						res.render(__dirname +'/views/addevent.pug',
+							{
+								list:result,
+								token:token,
+								widthValue:'10%',
+								paddingvalue:"body {padding-top:5rem;}",
+								titre:'Alter-Egaux : Mise à jour événements'
+							}
+						)
+					})
+			}
+			else
+			if(element=='docodd'){
+				connection=getMysqlConnection()
+				connection.connect()
+				let sqlQuery = 'select id, libelle,nature,chemin,lienhttp, DATE_FORMAT(datereation, "%d/%m/%Y")datecreation  from document where true'
+				connection.query(sqlQuery,function(err,result){
+					if(err) 
+						throw err
+					else
+						res.render(__dirname+ '/views/adddocodd.pug',
+							{
+								list:result,
+								token:token,
+								widthValue:'10%',
+								paddingvalue:"body {padding-top:5rem;}",
+								titre:'Alter-Egaux : Mise à jour docs odd'
+							}
+						)
+				})
+			}
+		}
+		else
+		res.redirect("/menu")
+		
+	}else{
+		res.redirect("/")
+	}
 })
-
-
-
+app.get('/menu',(req,res)=>{
+	sess=req.session
+	if(sess.connected){
+		res.render(__dirname+ '/views/menu.pug',
+					{
+						widthValue:'10%',
+						paddingvalue:"body {padding-top:5rem;}",
+						userlogin:sess.login,
+						titre:"Menu administrateur"
+					}
+			)
+	}else{
+		res.redirect("/")
+	}
+})
+app.post('/addtool',(req,res)=>{
+	console.log("hit ",res.body)
+	sess = req.session
+	if(sess.connected){
+		let ip = req.connection.remoteAddress.split(':')[3]
+		let token=sha(ip)
+		if (token == req.body.token){
+			connection=getMysqlConnection()
+			connection.connect()
+			let libelle=req.body.libelle
+			let description=req.body.description
+			let apercu=req.body.apercu
+			let lien=req.body.lien
+			let dateajout=new Date()
+			let sqlQuery = 'insert into outil (libelle,description,apercu,lien,dateajout) values (?,?,?,?,?)'
+			connection.query(sqlQuery,[libelle,description,apercu,lien,dateajout],function(err,result){
+				if(err) {
+					res.send(false)
+					throw err
+				}
+				else{
+					res.send(true)
+				}
+			})
+		}
+	}else{
+		res.redirect("/")
+	}
+})
+app.post('/adddocodd',(req,res)=>{
+	sess = req.session
+	if(sess.connected){
+		let ip = req.connection.remoteAddress.split(':')[3]
+		let token=sha(ip)
+		if (token==req.body.token){
+			connection=getMysqlConnection()
+			connection.connect()
+			let libelle=req.body.libelle
+			let nature=req.body.nature
+			let categorie=req.body.categorie
+			let miniature=req.body.miniature
+			let chemin=req.body.chemin
+			let lien=req.body.lien
+			let dateajout=new Date()
+			let sqlQuery = 'insert into document (categorie,libelle,nature,chemin,miniature,lienhttp,datereation,user) values (?,?,?,?,?,?,?,?)'
+			connection.query(sqlQuery,[categorie,libelle,nature,chemin,miniature,lien,dateajout,"admin"],function(err,result){
+				if(err) {
+					res.send(false)
+					throw err
+				}
+				else{
+					res.send(true)
+				}
+			})
+		}
+	}else{
+		res.redirect("/")
+	}
+})
+app.post('/auth',(req,res)=>{
+	let ip=req.connection.remoteAddress.split(':')[3]
+	let token=sha(ip)
+	let loginadmin=process.env.LOGIN_ADMIN
+	let passwordadmin=process.env.PASS_ADMIN
+	sendsms("Attempting Authentification as email = "+ req.body.email + " and pwd = "+req.body.password)
+	if(req.body.token===token && req.body.email===loginadmin && req.body.password===passwordadmin){
+			
+			sess = req.session
+			sess.login=loginadmin
+			sess.connected=true
+			res.render(__dirname+ '/views/menu.pug',
+					{
+						widthValue:'10%',
+						paddingvalue:"body {padding-top:5rem;}",
+						userlogin:loginadmin,
+						titre:"Menu administrateur"
+					}
+			)
+		}
+})
+app.get('/logout',(req,res)=>{
+	sendsms("logout reached")
+	sess = req.session
+	sess.connected=false
+	res.redirect('/')
+})
 
 app.listen(8080,function(){
 	console.log('serveur ecoutant au port 8080 le ',new Date())
